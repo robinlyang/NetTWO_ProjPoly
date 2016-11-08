f37e23fb9ff3052e689749c1415c8c102726e10f
robinlyang <robiny@gmail.com>
2016-11-07 20:07:23 -0700

    Initial Project

--- NetTWO_ProjPoly/Program.cs
+++ NetTWO_ProjPoly/Program.cs
@@ -6,10 +6,38 @@ using System.Threading.Tasks;
 
 namespace NetTWO_ProjPoly
 {
+    class sale
+    {
+        public double quantity, price;
+        public virtual double charge()
+        {
+            return quantity * price;
+        }
+        public double taxit()
+        {
+            return 0.05 * charge();
+        }
+        public void print()
+        {
+            Console.WriteLine(charge() + taxit());
+        }
+    }
+    class discount_sale : sale
+    {
+        public override double charge()
+        {
+            return 0.90 * quantity * price;
+        }
+    }
     class Program
     {
         static void Main(string[] args)
         {
+            discount_sale ds = new discount_sale();
+            ds.quantity = 3;
+            ds.price = 60;
+            ds.print();
+            Console.ReadLine();
         }
     }
 }
